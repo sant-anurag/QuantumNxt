971350b0db7aa05b92822162dda2a37d19e87450
Saurav Kokane <Saurav.kokane@sukritenvisonacademy>
2025-08-21 16:58:23 +0530

    updated command and database

--- install_commands.bat
+++ install_commands.bat
@@ -6,10 +6,11 @@ pip install mysqlclient
 pip install pandas
 pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org spacy
 pip install .\en_core_web_sm-3.8.0.tar.gz
-python.exe -m pip install pip<24.1
+python.exe -m pip install "pip<24.1"
 pip install textract
 python.exe -m pip install --upgrade pip
 pip install python-docx
 pip install pdfplumber==0.10.2
+echo # YOUR ENVIRONMENT VARIABLES > .env
 
 
