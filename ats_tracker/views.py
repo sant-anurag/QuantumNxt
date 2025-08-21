17bd5edeb25383ddd6a7e9466e86e558dc56a17f
deosanta <deo_santanurag@mail.com>
2025-08-21 17:38:29 +0530

    fas icons introduced for submenus

--- ats_tracker/views.py
+++ ats_tracker/views.py
@@ -28,9 +28,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.db import connection
-import environ
-
-
 
 
 def login_view(request):
